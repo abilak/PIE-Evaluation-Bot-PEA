7c916ec0c89a69647c003871c2f294c52a65cc6b
Aaditya <abilak@gmail.com>
2025-02-26 10:32:48 -0500

    Revert "Update pie.js"
    This reverts commit 6296cda010f0b361cae293e30403a2d4760b5aa1.

--- pie.js
+++ pie.js
@@ -1,18 +1,13 @@
-let classes = document.getElementsByClassName('list-group-item-heading breakall');
+eles = document.getElementsByClassName('inputGroupVertical');
 
-for(let j = 0; j < classess.length; j++) {
-	classes[j].click();
-	eles = document.getElementsByClassName('inputGroupVertical');
-
-	for(let i = 0; i < eles.length; i++) {
-		eles[i].children[0].children[0].click();
-	}
-	
-	let button = document.getElementById('surveysubmit');
-	button.click();
-	
-	let second_button = document.getElementsByClassName('btn btn-primary btn-lg btn-spinner')[2]
-	second_button.click();
+for(let i = 0; i < eles.length; i++) {
+	eles[i].children[0].children[0].click();
 }
+
+let button = document.getElementById('surveysubmit');
+button.click();
+
+let second_button = document.getElementsByClassName('btn btn-primary btn-lg btn-spinner')[2]
+second_button.click();
 
 
