a54ae57e285e399a4e2a7261a02fcf12faf1dee3
abilak <49570460+abilak@users.noreply.github.com>
2025-03-02 11:56:35 -0500

    Update pie.js

--- pie.js
+++ pie.js
@@ -1,13 +1,18 @@
-eles = document.getElementsByClassName('inputGroupVertical');
-
-for(let i = 0; i < eles.length; i++) {
-	eles[i].children[0].children[0].click();
+classes = document.getElementsbyClassName('list-group-item-heading breakall');
+for(let j = 0; j < classes.length; j++) {
+	classes[j].click() // edit later
+	eles = document.getElementsByClassName('inputGroupVertical');
+	for(let i = 0; i < eles.length; i++) {
+		eles[i].children[0].children[0].click();
+	}
+	
+	let button = document.getElementById('surveysubmit');
+	button.click();
+	
+	let second_button = document.getElementsByClassName('btn btn-primary btn-lg btn-spinner')[2]
+	second_button.click();
 }
 
-let button = document.getElementById('surveysubmit');
-button.click();
 
-let second_button = document.getElementsByClassName('btn btn-primary btn-lg btn-spinner')[2]
-second_button.click();
 
 
