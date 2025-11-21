961af40a7a220b8d0895a5536c4c5ab93d582240
abilak <49570460+abilak@users.noreply.github.com>
2025-11-20 19:45:37 -0500

    Refactor button click logic for survey navigation

--- pie.js
+++ pie.js
@@ -1,18 +1,14 @@
-classes = document.getElementsbyClassName('list-group-item-heading breakall');
-for(let j = 0; j < classes.length; j++) {
-	classes[j].click() // edit later
-	eles = document.getElementsByClassName('inputGroupVertical');
-	for(let i = 0; i < eles.length; i++) {
-		eles[i].children[0].children[0].click();
-	}
-	
-	let button = document.getElementById('surveysubmit');
-	button.click();
-	
-	let second_button = document.getElementsByClassName('btn btn-primary btn-lg btn-spinner')[2]
-	second_button.click();
+if(document.getElementById('NextButton').title == 'Submit') { 
+    document.getElementById('NextButton').click(); 
 }
 
+else { 
+    lister = document.getElementsByClassName('q-radio');
+    if(lister.length > 0) { 
+        for(let ele of lister) {
+            ele.click();
+        }
+    }
 
-
-
+    document.getElementById('NextButton').click(); 
+}
