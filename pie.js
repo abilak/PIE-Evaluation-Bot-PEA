4ee6e8ddc4546096a86fab196387ff489a80d26a
abilak <49570460+abilak@users.noreply.github.com>
2025-11-20 19:47:57 -0500

    Implement interval-based button click logic
    Refactor button click logic to execute at intervals.

--- pie.js
+++ pie.js
@@ -1,14 +1,18 @@
-if(document.getElementById('NextButton').title == 'Submit') { 
-    document.getElementById('NextButton').click(); 
-}
+setInterval(() => {
 
-else { 
-    lister = document.getElementsByClassName('q-radio');
-    if(lister.length > 0) { 
-        for(let ele of lister) {
-            ele.click();
-        }
+    if(document.getElementById('NextButton').title == 'Submit') { 
+        document.getElementById('NextButton').click(); 
     }
 
-    document.getElementById('NextButton').click(); 
-}
+    else { 
+        lister = document.getElementsByClassName('q-radio');
+        if(lister.length > 0) { 
+            for(let ele of lister) {
+                ele.click();
+            }
+        }
+
+        document.getElementById('NextButton').click(); 
+    }
+
+}, 1000);
